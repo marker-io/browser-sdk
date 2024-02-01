781774d1310ce1c40c602b4f0500ee2e25d52bef
Anto <seaflyscontact@gmail.com>
2024-02-01 14:39:54 +0100

    docs(exemples): Add nuxt 3 exemple

---
+++ examples/nuxt/app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup lang="ts">
+import markerSDK from '@marker.io/browser';
+
+const widget = await markerSDK.loadWidget({
+  project: 'YOUR_SITE_KEY_HERE',
+});
+</script>
